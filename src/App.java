12522c9e60801ed30643b5495a68e1e16b1c7168
Sameer <sk3577311@gmail.com>
2025-02-15 15:52:57 +0530

    second

--- src/App.java
+++ src/App.java
@@ -2,7 +2,8 @@ import programs.RecordsPrograms;
 
 public class App {
     public static void main(String[] args) {
-        RecordsPrograms rp = new RecordsPrograms("John doe", 12345);
-        System.out.println(rp);
+        RecordsPrograms rp = new RecordsPrograms("John", -9999);
+        System.out.println("Name: " + rp.name());
+        System.out.println("Emp id.: " + rp.employeeNumber());
     }
 }
--- src/programs/RecordsPrograms.java
+++ src/programs/RecordsPrograms.java
@@ -1,4 +1,15 @@
 package programs;
 
+// canonical constructor by default
+// records params are final params
+// cannot extends any other class
+// cannot create instance variable
+// implicitly final classes
+// it has compact constructor
 public record RecordsPrograms(String name, int employeeNumber) {
+    public RecordsPrograms {
+        if (employeeNumber < 0) {
+            throw new IllegalArgumentException("emp number should be positive.");
+        }
+    }
 }
